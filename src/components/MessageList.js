6018f7ea105b914e70ece30dad0b26e892e8b337
Panos K <lkullolli@gmail.com>
2018-07-02 19:14:24 +0400

    Looking for the solution

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Message from './Message';
-import '../App.css';
 
 class MessageList extends Component {
     render() {
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import '../App.css';
 
 class RoomList extends Component {
     componentWillUpdate() {
